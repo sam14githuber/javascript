54bc14deed3e61cba732dbaf42a2cf5adc3bc081
Samiksha Muley <152647917+sam14githuber@users.noreply.github.com>
2025-02-06 12:14:59 +0000

    datatypes conversion

---
+++ basics_1/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+//alert(2+2); we are using nodejs not browser
+//datatypes are - primitive (7) non primitive-(1) - nnbbssu
+//number - 23
+//null - standalone value
+//boolean - true/false
+//bigint - big integer used in stock
+//string - "hello"
+//symbol - unique
+//undefined - value is not defined
+
+//object
+let a ="hii"
+console.log(typeof a)
+
+//conversion to number
+//"33" = 33
+//"33abc" = NaN
+//true = 1  false=0
+
+//conversion to boolean
+//1 = true  0=false
+//""=false
+//"hitesh"= true
+
+let someNum = 33
+let stringnum = String(someNum)
+console.log(stringnum)
