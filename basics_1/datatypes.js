b90614a434dd2499eefba0185c541c27b83463cb
Samiksha Muley <152647917+sam14githuber@users.noreply.github.com>
2025-02-06 13:11:16 +0000

    dt summary

--- basics_1/datatypes.js
+++ basics_1/datatypes.js
@@ -1,6 +1,7 @@
 "use strict"; //treat all js code as newer version
 //alert(2+2); we are using nodejs not browser
-//datatypes are - primitive (7) non primitive-(1) - nnbbssu
+//datatypes are - primitive (7) - - nnbbssu 
+// non primitive-(3) - object, array, function - inn sabka datatype function aata h
 //number - 23
 //null - standalone value
 //boolean - true/false
@@ -26,3 +27,37 @@ console.log(typeof a)
 let someNum = 33
 let stringnum = String(someNum)
 console.log(stringnum)
+
+//OPERATIONS
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+//+ , -, *, /, %
+console.log(2**3)//power operator
+let str1 = "hii"
+let str2 = " sam"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+2+"2")//32
+console.log("1"+2+2)//122
+
+//array
+const arr = [2, 2 ,3, 4, 5];
+
+
+//object
+const obj= {
+     age: 5,
+     name: "Sam",
+
+}
+
+//Function
+const func= function(){
+    console.log("hello world");
+}
